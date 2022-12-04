81ceca8f378c3e771e0a1a01812b97b97c74bd3f
Peter Persson <pecka.persson@gmail.com>
2022-12-04 12:10:35 +0100

    Day 4

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -1,6 +1,4 @@
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection;
-using System.Runtime.ExceptionServices;
 
 namespace _2022;
 class Program
@@ -119,5 +117,28 @@ class Program
 
     Console.WriteLine(sumComps);
     Console.WriteLine(sumBadges);
+  }
+
+  public static void Day4()
+  {
+    List<string> inputs = File.ReadAllLines("./inputs/day4").ToList();
+
+    int fullyContainedCnt = 0;
+    int anyOverlapCnt = 0;
+    foreach (string input in inputs)
+    {
+      int[] elf1 = input.Split(',')[0].Split('-').Select(int.Parse).ToArray();
+      int[] elf2 = input.Split(',')[1].Split('-').Select(int.Parse).ToArray();
+
+      IEnumerable<int> elf1Range = Enumerable.Range(elf1[0], elf1[1] - elf1[0] + 1);
+      IEnumerable<int> elf2Range = Enumerable.Range(elf2[0], elf2[1] - elf2[0] + 1);
+      IEnumerable<int> intersect = elf1Range.Intersect(elf2Range);
+
+      if (intersect.SequenceEqual(elf1Range) || intersect.SequenceEqual(elf2Range)) fullyContainedCnt++;
+      if (intersect.Any()) anyOverlapCnt++;
+    }
+
+    Console.WriteLine(fullyContainedCnt);
+    Console.WriteLine(anyOverlapCnt);
   }
 }
