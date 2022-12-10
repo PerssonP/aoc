7e1c6d65a1ad7f97a12144aff524b4c76e9a1394
Peter Persson <pecka.persson@gmail.com>
2022-12-10 16:43:12 +0100

    Day 9

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -1,4 +1,8 @@
+using System.IO.Compression;
 using System.Reflection;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
 
 namespace _2022;
 class Program
@@ -325,7 +329,6 @@ class Program
         {
           amountE++;
           if (tree <= forest[k, y]) break;
-
         }
 
         for (int k = y - 1; k >= 0; k--) // North
@@ -345,6 +348,71 @@ class Program
     }
 
     Console.WriteLine(scores.Max());
+  }
+
+  public static void Day9()
+  {
+    static int solve(int amountOfTails)
+    {
+      string[] input = File.ReadAllLines("./inputs/day9.txt");
+
+      var headPos = (x: 0, y: 0);
+      var tailsPos = Enumerable.Repeat((x: 0, y: 0), amountOfTails).ToList();
+      HashSet<(int, int)> visitedPosSet = new HashSet<(int, int)> { tailsPos.Last() };
+
+      foreach (string line in input)
+      {
+        char direction = line[0];
+        int amount = int.Parse(line.Split(' ')[1]);
+        Func<(int x, int y), (int, int)> command = direction switch
+        {
+          'R' => pos => (pos.x + 1, pos.y),
+          'L' => pos => (pos.x - 1, pos.y),
+          'U' => pos => (pos.x, pos.y + 1),
+          'D' => pos => (pos.x, pos.y - 1),
+          _ => throw new Exception(),
+        };
+
+        foreach (int _ in Enumerable.Range(0, amount))
+        {
+          headPos = command.Invoke(headPos);
+
+          foreach (int i in Enumerable.Range(0, amountOfTails))
+          {
+            (int x, int y) tail = tailsPos[i];
+            (int x, int y) fauxHead = i == 0 ? headPos : tailsPos[i - 1]; // Head is either the actual head or the previous tail
+
+            int xDiff = Math.Abs(fauxHead.x - tail.x);
+            int yDiff = Math.Abs(fauxHead.y - tail.y);
+
+            if ((xDiff > 1 && yDiff > 0) || (xDiff > 0 && yDiff > 1))
+            {
+              // Tail needs to be moved diagonally
+              tail = (tail.x > fauxHead.x ? tail.x - 1 : tail.x + 1, tail.y > fauxHead.y ? tail.y - 1 : tail.y + 1);
+            }
+            else if (xDiff > 1)
+            {
+              // Head is directly east or west
+              tail = (tail.x > fauxHead.x ? tail.x - 1 : tail.x + 1, tail.y);
+            }
+            else if (yDiff > 1)
+            {
+              // Head is directly north or south
+              tail = (tail.x, tail.y > fauxHead.y ? tail.y - 1 : tail.y + 1);
+            }
+
+            tailsPos[i] = tail;
+          }
+
+          visitedPosSet.Add(tailsPos.Last());
+
+        }
+      }
+
+      return visitedPosSet.Count;
+    }
+
+    Console.WriteLine($"P1: {solve(1)} P2: {solve(9)}");
   }
 
 }
