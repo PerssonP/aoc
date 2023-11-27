3bdf4e76858779b7df1becfa7a7c58a5ed0942fa
Peter Persson <pecka.persson@gmail.com>
2023-11-27 12:15:55 +0100

    add 2023

---
+++ 2023/Program.cs
@@ -0,0 +1,16 @@
+﻿if (args.Length > 0 && int.TryParse(args[0], out int nbr))
+{
+    Type? type = Type.GetType($"Y2023.Day{nbr}");
+    if (type == null)
+    {
+        Console.WriteLine("Day not found");
+    }
+    else
+    {
+        type.GetMethod("Solve")!.Invoke(null, null);
+    }
+}
+else
+{
+    Console.WriteLine("Enter number of day to run as the first argument");
+}
